b98e0b07eacd4f88592241b64629925ad36a3da2
Mator <why101@msn.com>
2016-08-30 11:20:37 -0700

    refactored _element to _id in xeElement function definitions, simplified some function logic

--- xedit-lib/xeElements.pas
+++ xedit-lib/xeElements.pas
@@ -2,17 +2,17 @@ unit xeElements;
 
 interface
 
-  function GetElement(_element: Cardinal; key: PAnsiChar): Cardinal; StdCall;
-  function NewElement(_element: Cardinal; key: PAnsiChar): Cardinal; StdCall;
-  function RemoveElement(_element: Cardinal; key: PAnsiChar): WordBool; StdCall;
-  function ElementExists(_element: Cardinal; key: PAnsiChar): WordBool; StdCall;
-  function ElementCount(_element: Cardinal): Cardinal; StdCall;
-  function ElementAssigned(_element: Cardinal): Cardinal; StdCall;
-  function Equals(_element, _element2: Cardinal): WordBool; StdCall;
-  function IsMaster(_element: Cardinal): WordBool; StdCall;
-  function IsInjected(_element: Cardinal): WordBool; StdCall;
-  function IsOverride(_element: Cardinal): WordBool; StdCall;
-  function IsWinningOverride(_element: Cardinal): WordBool; StdCall;
+  function GetElement(_id: Cardinal; key: PAnsiChar): Cardinal; StdCall;
+  function NewElement(_id: Cardinal; key: PAnsiChar): Cardinal; StdCall;
+  function RemoveElement(_id: Cardinal; key: PAnsiChar): WordBool; StdCall;
+  function ElementExists(_id: Cardinal; key: PAnsiChar): WordBool; StdCall;
+  function ElementCount(_id: Cardinal): Cardinal; StdCall;
+  function ElementAssigned(_id: Cardinal): Cardinal; StdCall;
+  function Equals(_id, _id2: Cardinal): WordBool; StdCall;
+  function IsMaster(_id: Cardinal): WordBool; StdCall;
+  function IsInjected(_id: Cardinal): WordBool; StdCall;
+  function IsOverride(_id: Cardinal): WordBool; StdCall;
+  function IsWinningOverride(_id: Cardinal): WordBool; StdCall;
 
 implementation
 
@@ -39,13 +39,13 @@ end;
 
 // Replaces ElementByName, ElementByPath, ElementByIndex, GroupBySignature, and
 // ElementBySignature.  Supports indexed paths.
-function GetElement(_element: Cardinal; key: PAnsiChar): Cardinal; StdCall;
+function GetElement(_id: Cardinal; key: PAnsiChar): Cardinal; StdCall;
 var
   element: IInterface;
   _file: IwbFile;
   container: IwbContainerElementRef;
 begin
-  element := Resolve(_element);
+  element := Resolve(_id);
   if Supports(element, IwbFile, _file) then
     // TODO: perhaps also by group name?
     Result := Store(_file.GroupBySignature(StrToSignature(key)))
@@ -55,13 +55,13 @@ begin
 end;
 
 // replaces ElementAssign, Add, AddElement, and InsertElement
-function NewElement(_element: Cardinal; key: PAnsiChar): Cardinal; StdCall;
+function NewElement(_id: Cardinal; key: PAnsiChar): Cardinal; StdCall;
 var
   element: IInterface;
   container: IwbContainerElementRef;
   keyIndex: Integer;
 begin
-  element := Resolve(_element);
+  element := Resolve(_id);
   if Supports(element, IwbContainerElementRef, container) then begin
     // Use Add for files and groups
     if Supports(element, IwbFile) or Supports(element, IwbGroupRecord) then
@@ -82,22 +82,23 @@ begin
   end;
 end;
 
-function RemoveElement(_element: Cardinal; key: PAnsiChar): WordBool; StdCall;
+function RemoveElement(_id: Cardinal; key: PAnsiChar): WordBool; StdCall;
 var
   element: IInterface;
 begin
-  element := Resolve(_element);
+  Result := false;
+  element := Resolve(_id);
 end;
 
 // Replaces HasGroup and ElementExists
-function ElementExists(_element: Cardinal; key: PAnsiChar): WordBool; StdCall;
+function ElementExists(_id: Cardinal; key: PAnsiChar): WordBool; StdCall;
 var
   element: IInterface;
   _file: IwbFile;
   container: IwbContainerElementRef;
 begin
   Result := false;
-  element := Resolve(_element);
+  element := Resolve(_id);
   if Supports(element, IwbFile, _file) then
     // TODO: perhaps also by group name?
     Result := _file.HasGroup(StrToSignature(key))
@@ -106,75 +107,67 @@ begin
     Result := container.ElementExists[string(key)];
 end;
 
-function ElementCount(_element: Cardinal): Cardinal; StdCall;
+function ElementCount(_id: Cardinal): Cardinal; StdCall;
 var
-  element: IInterface;
   container: IwbContainerElementRef;
 begin
   Result := -1;
-  element := Resolve(_element);
-  if Supports(element, IwbContainerElementRef, container) then
+  if Supports(Resolve(_id), IwbContainerElementRef, container) then
     Result := container.ElementCount;
 end;
 
-function ElementAssigned(_element: Cardinal): Cardinal; StdCall;
+function ElementAssigned(_id: Cardinal): Cardinal; StdCall;
 var
   element: IInterface;
 begin
-  element := Resolve(_element);
+  element := Resolve(_id);
   Result := Assigned(element);
 end;
 
-function Equals(_element, _element2: Cardinal): WordBool; StdCall;
+function Equals(_id, _id2: Cardinal): WordBool; StdCall;
 var
   element, element2: IwbElement;
 begin
   Result := false;
-  if Supports(Resolve(_element), IwbElement, element) then
-    if Supports(Resolve(_element2), IwbElement, element2) then
+  if Supports(Resolve(_id), IwbElement, element) then
+    if Supports(Resolve(_id2), IwbElement, element2) then
       Result := element.Equals(element2);
 end;
 
-function IsMaster(_element: Cardinal): WordBool; StdCall;
+function IsMaster(_id: Cardinal): WordBool; StdCall;
 var
-  element: IInterface;
   rec: IwbMainRecord;
 begin
   Result := false;
-  element := Resolve(_element);
-  if Supports(element, IwbMainRecord, rec) then
+  if Supports(Resolve(_id), IwbMainRecord, rec) then
     Result := rec.IsMaster;
 end;
 
-function IsInjected(_element: Cardinal): WordBool; StdCall;
+function IsInjected(_id: Cardinal): WordBool; StdCall;
 var
-  element: IInterface;
   rec: IwbMainRecord;
 begin
   Result := false;
-  element := Resolve(_element);
-  if Supports(element, IwbMainRecord, rec) then
+  if Supports(Resolve(_id), IwbMainRecord, rec) then
     Result := rec.IsInjected;
 end;
 
-function IsOverride(_element: Cardinal): WordBool; StdCall;
+function IsOverride(_id: Cardinal): WordBool; StdCall;
 var
-  element: IInterface;
   rec: IwbMainRecord;
 begin
   Result := false;
-  element := Resolve(_element);
-  if Supports(element, IwbMainRecord, rec) then
+  if Supports(Resolve(_id), IwbMainRecord, rec) then
     Result := not rec.IsMaster;
 end;
 
 // TODO: Determine if subrecord is winner
-function IsWinningOverride(_element: Cardinal): WordBool; StdCall;
+function IsWinningOverride(_id: Cardinal): WordBool; StdCall;
 var
   rec: IwbMainRecord;
 begin
   Result := false;
-  if Supports(Resolve(_element), IwbMainRecord, rec) then
+  if Supports(Resolve(_id), IwbMainRecord, rec) then
     Result := not rec.IsWinningOverride;
 end;
 
