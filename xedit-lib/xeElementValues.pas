94e40fdd198e80b29aa60fd2a7d1c0fda48f1257
Mator <why101@msn.com>
2016-08-30 12:12:16 -0700

    added DefType and ElementType functions to xeElementValues

--- xedit-lib/xeElementValues.pas
+++ xedit-lib/xeElementValues.pas
@@ -7,6 +7,8 @@ interface
   function Signature(_id: Integer; str: PWideChar; len: Integer): WordBool; StdCall;
   function ShortName(_id: Integer; str: PWideChar; len: Integer): WordBool; StdCall;
   function SortKey(_id: Integer; str: PWideChar; len: Integer): WordBool; StdCall;
+  function ElementType(_id: Integer; str: PWideChar; len: Integer): WordBool; StdCall;
+  function DefType(_id: Integer; str: PWideChar; len: Integer): WordBool; StdCall;
 
 implementation
 
@@ -75,5 +77,76 @@ begin
   end;
 end;
 
+function etToString(et: Integer): String;
+begin
+  case et of
+    etFile: Result := 'etFile';
+    etMainRecord: Result := 'etMainRecord';
+    etMainRecord: Result := 'etMainRecord';
+    etGroupRecord: Result := 'etGroupRecord';
+    etSubRecord: Result := 'etSubRecord';
+    etSubRecordStruct: Result := 'etSubRecordStruct';
+    etSubRecordArray: Result := 'etSubRecordArray';
+    etSubRecordUnion: Result := 'etSubRecordUnion';
+    etArray: Result := 'etArray';
+    etStruct: Result := 'etStruct';
+    etValue: Result := 'etValue';
+    etFlag: Result := 'etFlag';
+    etStringListTerminator: Result := 'etStringListTerminator';
+    etUnion: Result := 'etFetUnionile';
+    etStructChapter: Result := 'etStructChapter';
+  end;
+end;
+
+function ElementType(_id: Integer; str: PWideChar; len: Integer): WordBool; StdCall;
+var
+  element: IwbElement;
+  s: String;
+begin
+  Result := false;
+  if Supports(Resolve(_id), IwbElement, element) then begin
+    s = etToString(element.ElementType);
+    StrLCopy(str, PWideChar(WideString(s)), len);
+    Result := true;
+  end;
+end;
+
+function dtToString(dt: Integer): String;
+begin
+  case dt of
+    dtRecord: Result := 'dtRecord';
+    dtSubRecord: Result := 'dtSubRecord';
+    dtSubRecordArray: Result := 'dtSubRecordArray';
+    dtSubRecordStruct: Result := 'dtSubRecordStruct';
+    dtSubRecordUnion: Result := 'dtSubRecordUnion';
+    dtString: Result := 'dtString';
+    dtLString: Result := 'dtLString';
+    dtLenString: Result := 'dtLenString';
+    dtByteArray: Result := 'dtByteArray';
+    dtInteger: Result := 'dtInteger';
+    dtIntegerFormater: Result := 'dtIntegerFormater';
+    dtIntegerFormaterUnion: Result := 'dtIntegerFormaterUnion';
+    dtFlag: Result := 'dtFlag';
+    dtFloat: Result := 'dtFloat';
+    dtArray: Result := 'dtArray';
+    dtStruct: Result := 'dtStruct';
+    dtUnion: Result := 'dtUnion';
+    dtEmpty: Result := 'dtEmpty';
+    dtStructChapter: Result := 'dtStructChapter';
+  end;
+end;
+
+function DefType(_id: Integer; str: PWideChar; len: Integer): WordBool; StdCall;
+var
+  element: IwbElement;
+  s: String;
+begin
+  Result := false;
+  if Supports(Resolve(_id), IwbElement, element) then begin
+    s := dtToString(element.ValueDef.DefType);
+    StrLCopy(str, PWideChar(WideString(s)), len);
+    Result := true;
+  end;
+end;
 
 end.
