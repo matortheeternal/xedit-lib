b8bd1a7fcd25876dbd3f825ced910c6fab2f258e
Mator <why101@msn.com>
2016-08-30 19:49:24 -0700

    added GetFileHeader function to xeFileValues

--- xedit-lib/xeFileValues.pas
+++ xedit-lib/xeFileValues.pas
@@ -2,6 +2,32 @@ unit xeFileValues;
 
 interface
 
+  function GetFileHeader(_id, _res: Cardinal): WordBool; StdCall;
 implementation
+
+uses
+  Classes, SysUtils, Variants,
+  // mte modules
+  mteHelpers,
+  // xedit modules
+  wbInterface, wbImplementation,
+  // xelib modules
+  xeMessages, xeMeta;
+
+
+function GetFileHeader(_id, _res: Cardinal): WordBool; StdCall;
+var
+  _file: IwbFile;
+begin
+  Result := false;
+  try
+    if Supports(Resolve(_id), IwbFile, _file) then begin
+      _res := Store(_file.Header);
+      Result := true;
+    end;
+  except
+    on x: Exception do ExceptionHandler(x);
+  end;
+end;
 
 end.
