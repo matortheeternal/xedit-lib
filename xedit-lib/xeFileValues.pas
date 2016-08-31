516e6f3d088a7a9ad539e32933f4e3919d452b80
Mator <why101@msn.com>
2016-08-30 19:50:22 -0700

    added GetAuthor function to xeFileValues

--- xedit-lib/xeFileValues.pas
+++ xedit-lib/xeFileValues.pas
@@ -5,6 +5,7 @@ interface
   function GetFileHeader(_id, _res: Cardinal): WordBool; StdCall;
   function GetNextObjectId(_id, nextObjectID: Cardinal): WordBool; StdCall;
   function SetNextObjectID(_id, nextObjectID: Cardinal): WordBool; StdCall;
+  function GetAuthor(_id: Cardinal; author: PWideChar; len: Integer): WordBool; StdCall;
 implementation
 
 uses
@@ -55,6 +56,23 @@ begin
   try
     if Supports(Resolve(_id), IwbFile, _file) then begin
       _file.Header.ElementNativeValues['HEDR\Next Object ID'] := nextObjectID;
+      Result := true;
+    end;
+  except
+    on x: Exception do ExceptionHandler(x);
+  end;
+end;
+
+function GetAuthor(_id: Cardinal; author: PWideChar; len: Integer): WordBool; StdCall;
+var
+  _file: IwbFile;
+  s: String;
+begin
+  Result := false;
+  try
+    if Supports(Resolve(_id), IwbFile, _file) then begin
+      s := _file.Header.ElementEditValues['CNAM'];
+      StrLCopy(author, PWideChar(WideString(s)), len);
       Result := true;
     end;
   except
