7bfee7fbb289acbafcd5e2732cd30b0eda6dcf5f
Mator <why101@msn.com>
2016-08-30 18:43:32 -0700

    implemented CleanMasters

--- xedit-lib/xeMasters.pas
+++ xedit-lib/xeMasters.pas
@@ -2,10 +2,10 @@ unit xeMasters;
 
 interface
 
-  function CleanMasters(_file: Cardinal): WordBool; StdCall;
   function SortMasters(_file: Cardinal): WordBool; StdCall;
   function AddMaster(_file, _master: Cardinal): WordBool; StdCall;
   function RemoveMaster(_file, _master: Cardinal): WordBool; StdCall;
+  function CleanMasters(_id: Cardinal): WordBool; StdCall;
 
 implementation
 
@@ -23,10 +23,18 @@ uses
 }
 {******************************************************************************}
 
-function CleanMasters(_file: Cardinal): WordBool; StdCall;
+function CleanMasters(_id: Cardinal): WordBool; StdCall;
+var
+  _file: IwbFile;
 begin
   Result := false;
-  // TODO
+  try
+    if Supports(Resolve(_id), IwbFile, _file) then
+      _file.CleanMasters;
+    Result := true;
+  except
+    on x: Exception do ExceptionHandler(x);
+  end;
 end;
 
 function SortMasters(_file: Cardinal): WordBool; StdCall;
