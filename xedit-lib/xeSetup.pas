e8b0bd881e000fc72292352163b9b1e5797ab33c
Mator <why101@msn.com>
2016-08-30 20:41:49 -0700

    created TLoaderThread in xeSetup

--- xedit-lib/xeSetup.pas
+++ xedit-lib/xeSetup.pas
@@ -3,10 +3,16 @@ unit xeSetup;
 interface
 
 uses
+  Classes,
   // xedit units
   wbInterface, wbImplementation;
 
-  // LOADING AND SETUP METHODS
+type
+  TLoaderThread = class(TThread)
+  protected
+    procedure Execute; override;
+  end;
+
   procedure SetGameMode(mode: Integer); StdCall;
   function GetLoadOrder(str: PAnsiChar; len: Integer): WordBool; StdCall;
   function LoadPlugins(str: PAnsiChar; len: Integer): WordBool; StdCall;
@@ -14,19 +20,72 @@ uses
 
 var
   Files: array of IwbFile;
+  slLoadOrder: TStringList;
 
 implementation
 
 uses
-  Classes, SysUtils,
-  // lib units
+  SysUtils,
+  // mte units
+  mteHelpers,
+  // xelib units
   xeMeta, xeConfiguration, xeMessages;
 
 {******************************************************************************}
 { LOADIND AND SETUP METHODS
-  Methods for loading and setting up the API.
+  Methods for setting the xEdit game mode and loading files.
 }
 {******************************************************************************}
+
+procedure TLoaderThread.Execute;
+var
+  i: Integer;
+  sFileName: String;
+  _file: IwbFile;
+begin
+  try
+    for i := 0 to Pred(slLoadOrder.Count) do begin
+      sFileName := slLoadOrder[i];
+      AddMessage(Format('Loading %s (%d/%d)', [sFileName, i + 1, slLoadOrder.Count]));
+
+      // load plugin
+      try
+        _file := wbFile(wbDataPath + sFileName, i, '', false, false);
+        _file._AddRef;
+        SetLength(Files, Length(Files) + 1);
+        Files[High(Files)] := _file;
+      except
+        on x: Exception do begin
+          AddMessage('Exception loading ' + sFileName);
+          AddMessage(x.Message);
+          raise x;
+        end;
+      end;
+
+      // load hardcoded dat
+      if i = 0 then try
+        _file := wbFile(Globals.Values['ProgramPath'] + wbGameName + wbHardcodedDat, 0);
+        _file._AddRef;
+        SetLength(Files, Length(Files) + 1);
+        Files[High(Files)] := _file;
+      except
+        on x: Exception do begin
+          AddMessage('Exception loading ' + wbGameName + wbHardcodedDat);
+          raise x;
+        end;
+      end;
+    end;
+
+    // done loading
+    ProgramStatus.bLoaderDone := true;
+    AddMessage('Done loading files.');
+  except
+    on E: Exception do begin
+      AddMessage('Fatal Error: <' + e.ClassName + ': ' + e.Message + '>');
+      wbLoaderError := true;
+    end;
+  end;
+end;
 
 procedure SetGameMode(mode: Integer); StdCall;
 begin
