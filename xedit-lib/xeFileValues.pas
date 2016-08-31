226c301deff1d30ba29776cafc3d1f6e9cc9ad72
Mator <why101@msn.com>
2016-08-30 19:49:43 -0700

    added GetNextObjectID function to xeFileValues

--- xedit-lib/xeFileValues.pas
+++ xedit-lib/xeFileValues.pas
@@ -3,6 +3,7 @@ unit xeFileValues;
 interface
 
   function GetFileHeader(_id, _res: Cardinal): WordBool; StdCall;
+  function GetNextObjectId(_id, nextObjectID: Cardinal): WordBool; StdCall;
 implementation
 
 uses
@@ -23,6 +24,21 @@ begin
   try
     if Supports(Resolve(_id), IwbFile, _file) then begin
       _res := Store(_file.Header);
+      Result := true;
+    end;
+  except
+    on x: Exception do ExceptionHandler(x);
+  end;
+end;
+
+function GetNextObjectId(_id, nextObjectID: Cardinal): WordBool; StdCall;
+var
+  _file: IwbFile;
+begin
+  Result := false;
+  try
+    if Supports(Resolve(_id), IwbFile, _file) then begin
+      nextObjectID := _file.Header.ElementNativeValues['HEDR\Next Object ID'];
       Result := true;
     end;
   except
