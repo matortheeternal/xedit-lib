467fda33ca0f1672cfaefe7061018a8c8b426ebd
Mator <why101@msn.com>
2016-11-01 20:33:51 -0700

    removed unneeded compiler directives from xeMessages

--- xedit-lib/xeMessages.pas
+++ xedit-lib/xeMessages.pas
@@ -15,18 +15,12 @@ implementation
 
 procedure AddMessage(msg: String);
 begin
-  {$IFDEF CONSOLE}
-  WriteLn(msg);
-  {$ELSE}
   MessageBuffer.Add(msg);
-  {$ENDIF}
 end;
 
 procedure SaveBuffer;
 begin
-  {$IFNDEF CONSOLE}
   MessageBuffer.SaveToFile('mod_dump_log.txt');
-  {$ENDIF}
 end;
 
 initialization
