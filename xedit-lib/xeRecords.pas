8168b991375e1b24f3c8d01dc2f1764c2da6fe12
Mator <why101@msn.com>
2016-11-02 10:20:06 -0700

    created RecordByName function in xeRecords

--- xedit-lib/xeRecords.pas
+++ xedit-lib/xeRecords.pas
@@ -8,6 +8,7 @@ interface
   //function RecordsBySignature(_id: Cardinal; sig: string; _res: PCardinalArray): WordBool; StdCall;
   function RecordByFormID(_id, formID: Cardinal; _res: PCardinal): WordBool; StdCall;
   function RecordByEditorID(_id: Cardinal; edid: string; _res: PCardinal): WordBool; StdCall;
+  function RecordByName(_id: Cardinal; full: string; _res: PCardinal): WordBool; StdCall;
   //function RecordSignatureFromName(name, str: PWideChar): WordBool; StdCall;
 
 implementation
@@ -102,6 +103,62 @@ begin
       rec := _file.RecordByEditorID[edid]
     else if Supports(e, IwbGroupRecord, _group) then
       rec := FindRecordByEditorID(_group, edid);
+    StoreIfAssigned(IInterface(rec), _res, Result);
+  except
+    on x: Exception do ExceptionHandler(x);
+  end;
+end;
+
+function FindRecordByName(_file: IwbFile; full: string): IwbMainRecord; overload;
+var
+  i: Integer;
+  rec: IwbMainRecord;
+  s: String;
+begin
+  for i := 0 to Pred(_file.RecordCount) do begin
+    rec := _file.Records[i];
+    s := rec.ElementEditValues['FULL'];
+    if SameText(s, full) then begin
+      Result := rec;
+      break;
+    end;
+  end;
+end;
+
+function FindRecordByName(group: IwbGroupRecord; full: string): IwbMainRecord; overload;
+var
+  i: Integer;
+  rec: IwbMainRecord;
+  s: String;
+begin
+  for i := 0 to Pred(group.ElementCount) do
+    if Supports(group.Elements[i], IwbMainRecord, rec) then begin
+      s := rec.ElementEditValues['FULL'];
+      if SameText(s, full) then begin
+        Result := rec;
+        break;
+      end;
+    end;
+end;
+
+function FindRecordByName(e: IInterface; full: string): IwbMainRecord; overload;
+var
+  _file: IwbFile;
+  group: IwbGroupRecord;
+begin
+  if Supports(e, IwbFile, _file) then
+    FindRecordByName(_file, full)
+  else if Supports(e, IwbGroupRecord, group) then
+    FindRecordByName(group, full);
+end;
+
+function RecordByName(_id: Cardinal; full: string; _res: PCardinal): WordBool; StdCall;
+var
+  rec: IwbMainRecord;
+begin
+  Result := false;
+  try
+    rec := FindRecordByName(Resolve(_id), full);
     StoreIfAssigned(IInterface(rec), _res, Result);
   except
     on x: Exception do ExceptionHandler(x);
