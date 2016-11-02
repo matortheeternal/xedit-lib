a58bc9a4980f2dd06d8cd61d65d49519f66c51df
Mator <why101@msn.com>
2016-11-02 15:57:49 -0700

    delphi test project now imports functions with the cdecl calling convention

--- test/Delphi/DelphiTest.dpr
+++ test/Delphi/DelphiTest.dpr
@@ -7,42 +7,42 @@ uses
   SysUtils;
 
   // META METHODS
-  procedure Initialize; StdCall; external 'XEditLib.dll';
-  procedure Finalize; StdCall; external 'XEditLib.dll';
-  procedure GetBuffer(str: PWideChar; len: Integer); StdCall; external 'XEditLib.dll';
-  procedure FlushBuffer; StdCall; external 'XEditLib.dll';
-  function GetExceptionMessage(str: PWideChar; len: Integer): WordBool; StdCall; external 'XEditLib.dll';
-  function GetGlobal(key, value: PWideChar; len: Integer): WordBool; StdCall; external 'XEditLib.dll';
-  procedure Release(_id: Cardinal); StdCall; external 'XEditLib.dll';
-  procedure ResetStore; StdCall; external 'XEditLib.dll';
+  procedure Initialize; cdecl; external 'XEditLib.dll';
+  procedure Finalize; cdecl; external 'XEditLib.dll';
+  procedure GetBuffer(str: PWideChar; len: Integer); cdecl; external 'XEditLib.dll';
+  procedure FlushBuffer; cdecl; external 'XEditLib.dll';
+  function GetExceptionMessage(str: PWideChar; len: Integer): WordBool; cdecl; external 'XEditLib.dll';
+  function GetGlobal(key, value: PWideChar; len: Integer): WordBool; cdecl; external 'XEditLib.dll';
+  procedure Release(_id: Cardinal); cdecl; external 'XEditLib.dll';
+  procedure ResetStore; cdecl; external 'XEditLib.dll';
 
   // LOADING AND SET UP METHODS
-  procedure SetGameMode(mode: Integer); StdCall; external 'XEditLib.dll';
-  function GetLoadOrder(str: PWideChar; len: Integer): WordBool; StdCall; external 'XEditLib.dll';
-  function LoadPlugins(loadOrder: PWideChar): WordBool; StdCall; StdCall; external 'XEditLib.dll';
-  function GetLoaderDone: WordBool; StdCall; external 'XEditLib.dll';
+  procedure SetGameMode(mode: Integer); cdecl; external 'XEditLib.dll';
+  function GetLoadOrder(str: PWideChar; len: Integer): WordBool; cdecl; external 'XEditLib.dll';
+  function LoadPlugins(loadOrder: PWideChar): WordBool; cdecl; cdecl; external 'XEditLib.dll';
+  function GetLoaderDone: WordBool; cdecl; external 'XEditLib.dll';
 
   // FILE HANDLING METHODS
-  function NewFile(filename: PAnsiChar): Cardinal; StdCall; external 'XEditLib.dll';
-  function FileByIndex(index: Integer): Cardinal; StdCall; external 'XEditLib.dll';
-  function FileByLoadOrder(load_order: Integer): Cardinal; StdCall; external 'XEditLib.dll';
-  function FileByName(name: PAnsiChar): Cardinal; StdCall; external 'XEditLib.dll';
-  function FileByAuthor(author: PAnsiChar): Cardinal; StdCall; external 'XEditLib.dll';
-  function GetElementFile(_id: Cardinal): Cardinal; StdCall; external 'XEditLib.dll';
-  function SaveFile(_id: Cardinal): WordBool; StdCall; external 'XEditLib.dll';
+  function NewFile(filename: PAnsiChar): Cardinal; cdecl; external 'XEditLib.dll';
+  function FileByIndex(index: Integer): Cardinal; cdecl; external 'XEditLib.dll';
+  function FileByLoadOrder(load_order: Integer): Cardinal; cdecl; external 'XEditLib.dll';
+  function FileByName(name: PAnsiChar): Cardinal; cdecl; external 'XEditLib.dll';
+  function FileByAuthor(author: PAnsiChar): Cardinal; cdecl; external 'XEditLib.dll';
+  function GetElementFile(_id: Cardinal): Cardinal; cdecl; external 'XEditLib.dll';
+  function SaveFile(_id: Cardinal): WordBool; cdecl; external 'XEditLib.dll';
 
   // FILE VALUE METHODS
-  function GetFileHeader(_id, _res: Cardinal): WordBool; StdCall; external 'XEditLib.dll';
-  function GetNextObjectId(_id, nextObjectID: Cardinal): WordBool; StdCall; external 'XEditLib.dll';
-  function SetNextObjectID(_id, nextObjectID: Cardinal): WordBool; StdCall; external 'XEditLib.dll';
-  function GetFileName(_id: Cardinal; fileName: PWideChar; len: Integer): WordBool; StdCall; external 'XEditLib.dll';
-  function GetAuthor(_id: Cardinal; author: PWideChar; len: Integer): WordBool; StdCall; external 'XEditLib.dll';
-  function SetAuthor(_id: Cardinal; author: PWideChar): WordBool; StdCall; external 'XEditLib.dll';
-  function GetDescription(_id: Cardinal; desc: PWideChar; len: Integer): WordBool; StdCall; external 'XEditLib.dll';
-  function SetDescription(_id: Cardinal; desc: PWideChar): WordBool; StdCall; external 'XEditLib.dll';
-  function OverrideRecordCount(_id: Cardinal; count: Integer): WordBool; StdCall; external 'XEditLib.dll';
-  function GetIsESM(_id: Cardinal; isESM: PWordBool): WordBool; StdCall; external 'XEditLib.dll';
-  function SetIsESM(_id: Cardinal; isESM: WordBool): WordBool; StdCall; external 'XEditLib.dll';
+  function GetFileHeader(_id, _res: Cardinal): WordBool; cdecl; external 'XEditLib.dll';
+  function GetNextObjectId(_id, nextObjectID: Cardinal): WordBool; cdecl; external 'XEditLib.dll';
+  function SetNextObjectID(_id, nextObjectID: Cardinal): WordBool; cdecl; external 'XEditLib.dll';
+  function GetFileName(_id: Cardinal; fileName: PWideChar; len: Integer): WordBool; cdecl; external 'XEditLib.dll';
+  function GetAuthor(_id: Cardinal; author: PWideChar; len: Integer): WordBool; cdecl; external 'XEditLib.dll';
+  function SetAuthor(_id: Cardinal; author: PWideChar): WordBool; cdecl; external 'XEditLib.dll';
+  function GetDescription(_id: Cardinal; desc: PWideChar; len: Integer): WordBool; cdecl; external 'XEditLib.dll';
+  function SetDescription(_id: Cardinal; desc: PWideChar): WordBool; cdecl; external 'XEditLib.dll';
+  function OverrideRecordCount(_id: Cardinal; count: Integer): WordBool; cdecl; external 'XEditLib.dll';
+  function GetIsESM(_id: Cardinal; isESM: PWordBool): WordBool; cdecl; external 'XEditLib.dll';
+  function SetIsESM(_id: Cardinal; isESM: WordBool): WordBool; cdecl; external 'XEditLib.dll';
 
 
 procedure WriteBuffer;
