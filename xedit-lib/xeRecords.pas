c50c42879f856e1f12452e86728104516c2b78f3
Mator <why101@msn.com>
2016-11-02 10:14:07 -0700

    created RecordByFormID function in xeRecords

--- xedit-lib/xeRecords.pas
+++ xedit-lib/xeRecords.pas
@@ -5,6 +5,8 @@ interface
   function AddRecord(_id: Cardinal; sig: string; _res: PCardinal): WordBool; StdCall;
   //function GetRecords(_id: Cardinal; _res: PCardinalArray): WordBool; StdCall;
   function RecordByIndex(_id: Cardinal; index: Integer; _res: PCardinal): WordBool; StdCall;
+  //function RecordsBySignature(_id: Cardinal; sig: string; _res: PCardinalArray): WordBool; StdCall;
+  function RecordByFormID(_id, formID: Cardinal; _res: PCardinal): WordBool; StdCall;
   //function RecordSignatureFromName(name, str: PWideChar): WordBool; StdCall;
 
 implementation
@@ -50,6 +52,22 @@ begin
     else if Supports(e, IwbGroupRecord, _group) then begin
       _res^ := Store(_group.Elements[index]);
       Result := true;
+    end;
+  except
+    on x: Exception do ExceptionHandler(x);
+  end;
+end;
+
+function RecordByFormID(_id, formID: Cardinal; _res: PCardinal): WordBool; StdCall;
+var
+  _file: IwbFile;
+  rec: IwbMainRecord;
+begin
+  Result := false;
+  try
+    if Supports(Resolve(_id), IwbFile, _file) then begin
+      rec := _file.RecordByFormID[formID, true];
+      StoreIfAssigned(IInterface(rec), _res, Result);
     end;
   except
     on x: Exception do ExceptionHandler(x);
