6f9fb8f79f16d5da92ffddb860d2a4d43c03eb21
Mator <why101@msn.com>
2016-11-02 10:13:27 -0700

    created AddRecord function in xeRecords

--- xedit-lib/xeRecords.pas
+++ xedit-lib/xeRecords.pas
@@ -2,8 +2,34 @@ unit xeRecords;
 
 interface
 
-  //function RecordSignatureFromName(name, str: PWideChar; len: Integer): WordBool; StdCall;
+  function AddRecord(_id: Cardinal; sig: string; _res: PCardinal): WordBool; StdCall;
+  //function RecordSignatureFromName(name, str: PWideChar): WordBool; StdCall;
 
 implementation
+
+uses
+  Classes, SysUtils,
+  // xedit units
+  wbInterface, wbImplementation,
+  // xelib units
+  xeGroups, xeMeta;
+
+function AddRecord(_id: Cardinal; sig: string; _res: PCardinal): WordBool; StdCall;
+var
+  _file: IwbFile;
+  group: IwbGroupRecord;
+  element: IwbElement;
+begin
+  Result := false;
+  try
+    if Supports(Resolve(_id), IwbFile, _file) then begin
+      group := AddGroupIfMissing(_file, sig);
+      element := group.Add(sig);
+      StoreIfAssigned(IInterface(element), _res, Result);
+    end;
+  except
+    on x: Exception do ExceptionHandler(x);
+  end;
+end;
 
 end.
