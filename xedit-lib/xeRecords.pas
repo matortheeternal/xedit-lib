c8398a9cf988986b0822a3eb3609ea222d674774
Mator <why101@msn.com>
2016-11-02 10:13:43 -0700

    created RecordByIndex function in xeRecords

--- xedit-lib/xeRecords.pas
+++ xedit-lib/xeRecords.pas
@@ -3,6 +3,8 @@ unit xeRecords;
 interface
 
   function AddRecord(_id: Cardinal; sig: string; _res: PCardinal): WordBool; StdCall;
+  //function GetRecords(_id: Cardinal; _res: PCardinalArray): WordBool; StdCall;
+  function RecordByIndex(_id: Cardinal; index: Integer; _res: PCardinal): WordBool; StdCall;
   //function RecordSignatureFromName(name, str: PWideChar): WordBool; StdCall;
 
 implementation
@@ -26,6 +28,28 @@ begin
       group := AddGroupIfMissing(_file, sig);
       element := group.Add(sig);
       StoreIfAssigned(IInterface(element), _res, Result);
+    end;
+  except
+    on x: Exception do ExceptionHandler(x);
+  end;
+end;
+
+function RecordByIndex(_id: Cardinal; index: Integer; _res: PCardinal): WordBool; StdCall;
+var
+  e: IInterface;
+  _file: IwbFile;
+  _group: IwbGroupRecord;
+begin
+  Result := false;
+  try
+    e := Resolve(_id);
+    if Supports(e, IwbFile, _file) then begin
+      _res^ := Store(_file.Records[index]);
+      Result := true;
+    end
+    else if Supports(e, IwbGroupRecord, _group) then begin
+      _res^ := Store(_group.Elements[index]);
+      Result := true;
     end;
   except
     on x: Exception do ExceptionHandler(x);
