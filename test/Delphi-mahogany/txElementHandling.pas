bea61a1c548f535bb22c512ece5f5dc14d9b8c66
Mator <why101@msn.com>
2016-12-21 18:04:37 -0800

    added GetElement tests for File element resolution by index

--- test/Delphi-mahogany/txElementHandling.pas
+++ test/Delphi-mahogany/txElementHandling.pas
@@ -166,6 +166,21 @@ begin
                   Expect(not success, 'Result should be false');
                 end);
             end);
+
+          Describe('File element resolution by index', procedure
+            begin
+              It('Should return a handle if the index is in bounds', procedure
+                begin
+                  GetElement(skyrim, '[0]', @h);
+                  Expect(h > 0, 'Handle should be greater than 0');
+                end);
+
+              It('Should fail if index is out of bounds', procedure
+               begin
+                 success := GetElement(skyrim, '[-1]', @h);
+                 Expect(not success, 'Result should be false');
+               end);
+            end);
         end);
     end);
 end;
