1381bf8b9fc1804f02a52caa9d5701fdc5e5b3cb
Mator <why101@msn.com>
2017-01-03 19:45:02 -0800

    adjusted some function declarations in txElementValues

--- test/Delphi-mahogany/txElementValues.pas
+++ test/Delphi-mahogany/txElementValues.pas
@@ -13,17 +13,17 @@ interface
   function DefType(_id: Integer; str: PWideChar; len: Integer): WordBool; cdecl; external 'XEditLib.dll';
   function GetValue(_id: Integer; path, str: PWideChar; len: Integer): WordBool; cdecl; external 'XEditLib.dll';
   function SetValue(_id: Integer; path, value: PWideChar): WordBool; cdecl; external 'XEditLib.dll';
-  function GetIntValue(_id: Integer; path: PWideChar; out value: Integer): WordBool; cdecl; external 'XEditLib.dll';
+  function GetIntValue(_id: Integer; path: PWideChar; value: PInteger): WordBool; cdecl; external 'XEditLib.dll';
   function SetIntValue(_id: Integer; path: PWideChar; value: Integer): WordBool; cdecl; external 'XEditLib.dll';
-  function GetUIntValue(_id: Integer; path: PWideChar; out value: Cardinal): WordBool; cdecl; external 'XEditLib.dll';
+  function GetUIntValue(_id: Integer; path: PWideChar; value: PCardinal): WordBool; cdecl; external 'XEditLib.dll';
   function SetUIntValue(_id: Integer; path: PWideChar; value: Cardinal): WordBool; cdecl; external 'XEditLib.dll';
-  function GetFloatValue(_id: Integer; path: PWideChar; out value: Double): WordBool; cdecl; external 'XEditLib.dll';
+  function GetFloatValue(_id: Integer; path: PWideChar; value: PDouble): WordBool; cdecl; external 'XEditLib.dll';
   function SetFloatValue(_id: Integer; path: PWideChar; value: Double): WordBool; cdecl; external 'XEditLib.dll';
   function GetLinksTo(_id: Integer; path: PWideChar; _res: PCardinal): WordBool; cdecl; external 'XEditLib.dll';
   function SetFlag(_id: Integer; path, name: PWideChar; enabled: WordBool): WordBool; cdecl; external 'XEditLib.dll';
   function GetFlag(_id: Integer; path, name: PWideChar): WordBool; cdecl; external 'XEditLib.dll';
   function ToggleFlag(_id: Integer; path, name: PWideChar): WordBool; cdecl; external 'XEditLib.dll';
-  function GetEnabledFlags(_id: Integer; path: PWideChar; out flags: PWideChar): WordBool; cdecl; external 'XEditLib.dll';
+  function GetEnabledFlags(_id: Integer; path, flags: PWideChar): WordBool; cdecl; external 'XEditLib.dll';
 
   // PUBLIC TESTING INTERFACE
   procedure BuildElementValueTests;
