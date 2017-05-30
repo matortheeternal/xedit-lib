991caaf30c09aea5ea31990eb8a4d7f0aaf7bbb0
Mator <why101@msn.com>
2017-05-29 18:23:59 -0700

    refactored xeRecords methods to raise more useful exceptions, removed RecordByIndex, completed FindRecordsBySignature

--- src/xeRecords.pas
+++ src/xeRecords.pas
@@ -3,12 +3,12 @@ unit xeRecords;
 interface
 
 uses
+  wbInterface,
   xeMeta;
 
   function AddRecord(_id: Cardinal; sig: PWideChar; _res: PCardinal): WordBool; cdecl;
   function GetRecords(_id: Cardinal; len: PInteger): WordBool; cdecl;
   function RecordsBySignature(_id: Cardinal; sig: PWideChar; len: PInteger): WordBool; cdecl;
-  function RecordByIndex(_id: Cardinal; index: Integer; _res: PCardinal): WordBool; cdecl;
   function RecordByFormID(_id, formID: Cardinal; _res: PCardinal): WordBool; cdecl;
   function RecordByEditorID(_id: Cardinal; edid: PWideChar; _res: PCardinal): WordBool; cdecl;
   function RecordByName(_id: Cardinal; full: PWideChar; _res: PCardinal): WordBool; cdecl;
@@ -22,34 +22,41 @@ uses
   function IsOverride(_id: Cardinal; bool: PWordBool): WordBool; cdecl;
   function IsWinningOverride(_id: Cardinal; bool: PWordBool): WordBool; cdecl;
 
+  // NATIVE FUNCTIONS
+  procedure StoreRecords(_file: IwbFile; len: PInteger); overload;
+  procedure StoreRecords(group: IwbGroupRecord; len: PInteger); overload;
+  procedure FindRecordsBySignature(group: IwbGroupRecord; sig: TwbSignature; len: PInteger); overload;
+  procedure FindRecordsBySignature(_file: IwbFile; sig: TwbSignature; len: PInteger); overload;
+
 implementation
 
 uses
   Classes, SysUtils,
-  // xedit units
-  wbInterface, wbImplementation,
-  // xelib units
+  wbImplementation,
   xeGroups, xeMessages;
 
 function AddRecord(_id: Cardinal; sig: PWideChar; _res: PCardinal): WordBool; cdecl;
 var
+  e: IInterface;
   _file: IwbFile;
   group: IwbGroupRecord;
   element: IwbElement;
 begin
   Result := False;
   try
-    if Supports(Resolve(_id), IwbFile, _file) then begin
-      group := AddGroupIfMissing(_file, string(sig));
-      element := group.Add(string(sig));
-      StoreIfAssigned(IInterface(element), _res, Result);
-    end;
+    e := Resolve(_id);
+    if Supports(e, IwbFile, _file) then
+      group := AddGroupIfMissing(_file, string(sig))
+    else if not Supports(e, IwbGroupRecord, group) then
+      raise Exception.Create('Interface must be a file or group');
+    element := group.Add(string(sig));
+    StoreIfAssigned(IInterface(element), _res, Result);
   except
     on x: Exception do ExceptionHandler(x);
   end;
 end;
 
-procedure StoreRecords(_file: IwbFile; len: PInteger); overload;
+procedure StoreRecords(_file: IwbFile; len: PInteger);
 var
   i: Integer;
 begin
@@ -59,7 +66,7 @@ begin
     resultArray[i] := Store(_file.Records[i]);
 end;
 
-procedure StoreRecords(group: IwbGroupRecord; len: PInteger); overload;
+procedure StoreRecords(group: IwbGroupRecord; len: PInteger);
 var
   i: Integer;
   rec: IwbMainRecord;
@@ -82,62 +89,83 @@ begin
   Result := False;
   try
     e := Resolve(_id);
-    if Supports(e, IwbFile, _file) then begin
-      StoreRecords(_file, len);
-      Result := True;
-    end
-    else if Supports(e, IwbGroupRecord, group) then begin
-      StoreRecords(group, len);
-      Result := True;
-    end;
+    if Supports(e, IwbFile, _file) then
+      StoreRecords(_file, len)
+    else if Supports(e, IwbGroupRecord, group) then
+      StoreRecords(group, len)
+    else
+      raise Exception.Create('Interface must be a file or group.');
+    Result := True;
   except
     on x: Exception do ExceptionHandler(x);
   end;
+end;
+
+procedure StoreRecord(rec: IwbMainRecord; len: PInteger);
+var
+  capacity: Integer;
+begin
+  // grow capacity by 1KB when reached
+  capacity := High(resultArray);
+  if len^ > capacity then
+    SetLength(resultArray, capacity + 256);
+  resultArray[len^] := Store(IInterface(rec));
+  Inc(len^);
+end;
+
+procedure FindRecordsBySignature(group: IwbGroupRecord; sig: TwbSignature; len: PInteger);
+var
+  i: Integer;
+  element: IwbElement;
+  rec: IwbMainRecord;
+  subGroup: IwbGroupRecord;
+begin
+  if (group.GroupType = 0) and (TwbSignature(group.GroupLabel) = sig) then
+    StoreRecords(group, len)
+  else
+    for i := 0 to Pred(group.ElementCount) do begin
+      element := group.Elements[i];
+      if Supports(element, IwbMainRecord, rec) then begin
+        if rec.Signature = sig then
+          StoreRecord(rec, len)
+        else if Assigned(rec.ChildGroup) then
+          FindRecordsBySignature(rec.ChildGroup, sig, len);   
+      end
+      else if Supports(element, IwbGroupRecord, subGroup) then
+        FindRecordsBySignature(subGroup, sig, len);   
+    end;
+end;
+
+procedure FindRecordsBySignature(_file: IwbFile; sig: TwbSignature; len: PInteger);
+var
+  i: Integer;
+  group: IwbGroupRecord;
+begin
+  if _file.HasGroup(sig) then
+    StoreRecords(_file.GroupBySignature[sig], len)
+  else
+    for i := 0 to _file.ElementCount do
+      if Supports(_file.Elements[i], IwbGroupRecord, group) then
+        FindRecordsBySignature(group, sig, len);
 end;
 
 function RecordsBySignature(_id: Cardinal; sig: PWideChar; len: PInteger): WordBool; cdecl;
 var
   _sig: TwbSignature;
   _file: IwbFile;
-  i: Integer;
   group: IwbGroupRecord;
-  rec: IwbMainRecord;
 begin
   Result := False;
   try
+    len^ := 0;
     _sig := TwbSignature(AnsiString(sig));
-    if Supports(Resolve(_id), IwbFile, _file) and _file.HasGroup(_sig) then begin
-      group := _file.GroupBySignature[_sig];
-      len^ := 0;
-      SetLength(resultArray, group.ElementCount);
-      for i := 0 to Pred(group.ElementCount) do
-        if Supports(group.Elements[i], IwbMainRecord, rec) then begin
-          resultArray[len^] := Store(group.Elements[i]);
-          Inc(len^);
-        end;
-    end;
-  except
-    on x: Exception do ExceptionHandler(x);
-  end;
-end;
-
-function RecordByIndex(_id: Cardinal; index: Integer; _res: PCardinal): WordBool; cdecl;
-var
-  e: IInterface;
-  _file: IwbFile;
-  _group: IwbGroupRecord;
-begin
-  Result := False;
-  try
-    e := Resolve(_id);
-    if Supports(e, IwbFile, _file) then begin
-      _res^ := Store(_file.Records[index]);
-      Result := True;
-    end
-    else if Supports(e, IwbGroupRecord, _group) then begin
-      _res^ := Store(_group.Elements[index]);
-      Result := True;
-    end;
+    if Supports(Resolve(_id), IwbFile, _file) then
+      FindRecordsBySignature(_file, _sig, len)
+    else if Supports(Resolve(_id), IwbGroupRecord, group) then
+      FindRecordsBySignature(group, _sig, len)
+    else
+      raise Exception.Create('Interface must be a file or group.');
+    Result := True;
   except
     on x: Exception do ExceptionHandler(x);
   end;
@@ -150,10 +178,10 @@ var
 begin
   Result := False;
   try
-    if Supports(Resolve(_id), IwbFile, _file) then begin
-      rec := _file.RecordByFormID[formID, True];
-      StoreIfAssigned(IInterface(rec), _res, Result);
-    end;
+    if not Supports(Resolve(_id), IwbFile, _file) then
+      raise Exception.Create('Interface must be a file.');
+    rec := _file.RecordByFormID[formID, True];
+    StoreIfAssigned(IInterface(rec), _res, Result);
   except
     on x: Exception do ExceptionHandler(x);
   end;
@@ -185,7 +213,9 @@ begin
     if Supports(e, IwbFile, _file) then
       rec := _file.RecordByEditorID[string(edid)]
     else if Supports(e, IwbGroupRecord, _group) then
-      rec := FindRecordByEditorID(_group, string(edid));
+      rec := FindRecordByEditorID(_group, string(edid))
+    else
+      raise Exception.Create('Input interface must be a file or a group.');
     StoreIfAssigned(IInterface(rec), _res, Result);
   except
     on x: Exception do ExceptionHandler(x);
@@ -269,10 +299,10 @@ var
 begin
   Result := False;
   try
-    if Supports(Resolve(_id), IwbMainRecord, rec) then begin
-      formID^ := rec.LoadOrderFormID;
-      Result := True;
-    end;
+    if not Supports(Resolve(_id), IwbMainRecord, rec) then
+      raise Exception.Create('Interface must be a main record.');
+    formID^ := rec.LoadOrderFormID;
+    Result := True;
   except
     on x: Exception do ExceptionHandler(x);
   end;
@@ -285,10 +315,10 @@ var
 begin
   Result := False;
   try
-    if Supports(Resolve(_id), IwbMainRecord, rec) then begin
-      rec.LoadOrderFormID := formID;
-      Result := True;
-    end;
+    if not Supports(Resolve(_id), IwbMainRecord, rec) then
+      raise Exception.Create('Interface must be a main record.');
+    rec.LoadOrderFormID := formID;
+    Result := True;
   except
     on x: Exception do ExceptionHandler(x);
   end;
@@ -300,10 +330,10 @@ var
 begin
   Result := False;
   try
-    if Supports(Resolve(_id), IwbMainRecord, rec) then begin
-      rec.CompareExchangeFormID(oldFormID, newFormID);
-      Result := True;
-    end;
+    if not Supports(Resolve(_id), IwbMainRecord, rec) then
+      raise Exception.Create('Interface must be a main record.');
+    rec.CompareExchangeFormID(oldFormID, newFormID);
+    Result := True;
   except
     on x: Exception do ExceptionHandler(x);
   end;
@@ -316,14 +346,14 @@ var
 begin
   Result := False;
   try
-    if Supports(Resolve(_id), IwbMainRecord, rec) then begin
-      len^ := rec.ReferencedByCount;
-      SetLength(resultArray, len^);
-      for i := 0 to Pred(rec.ReferencedByCount) do
-        if Supports(rec.ReferencedBy[i], IwbMainRecord, ref) then
-          resultArray[i] := Store(ref);
-      Result := True;
-    end;
+    if not Supports(Resolve(_id), IwbMainRecord, rec) then
+      raise Exception.Create('Interface must be a main record.');
+    len^ := rec.ReferencedByCount;
+    SetLength(resultArray, len^);
+    for i := 0 to Pred(rec.ReferencedByCount) do
+      if Supports(rec.ReferencedBy[i], IwbMainRecord, ref) then
+        resultArray[i] := Store(ref);
+    Result := True;
   except
     on x: Exception do ExceptionHandler(x);
   end;
