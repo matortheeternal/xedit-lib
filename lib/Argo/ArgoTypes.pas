669279735ffcfad85e75e9715fe4ca811863df80
Mator <why101@msn.com>
2017-06-02 15:07:34 -0700

    added IndexOfValue function to TFastStringList in ArgoTypes

--- lib/Argo/ArgoTypes.pas
+++ lib/Argo/ArgoTypes.pas
@@ -8,7 +8,9 @@ uses
 type
   TFastStringList = class(TStringList)
   protected
-    function CompareStrings(const S1, S2: string): Integer; override;
+    function CompareStrings(const S1, S2: String): Integer; override;
+  public
+    function IndexOfValue(const Value: String): Integer;
   end;
 
   PArgoTreeNode = ^TArgoTreeNode;
@@ -52,6 +54,20 @@ uses
 function TFastStringList.CompareStrings(const S1, S2: string): Integer;
 begin
   Result := CompareStr(S1, S2);
+end;
+
+function TFastStringList.IndexOfValue(const Value: string): Integer;
+var
+  P: Integer;
+  S: string;
+begin
+  for Result := 0 to GetCount - 1 do
+  begin
+    S := Get(Result);
+    P := AnsiPos(NameValueSeparator, S);
+    if (P <> 0) and (CompareStrings(Copy(S, P + 1, Length(S)), Value) = 0) then Exit;
+  end;
+  Result := -1;
 end;
 
 { TBinaryTree }
