b3c880b2a1c02ce6cb33dd1f2d28964611422acc
narukaze <bigguy132l@gmail.com>
2017-06-17 16:00:01 -0400

    Set up txMaster.pas

--- tests/txMasters.pas
+++ tests/txMasters.pas
@@ -2,14 +2,40 @@ unit txMasters;
 
 interface
 
+uses
+  SysUtils;
+
   // PUBLIC TESTING INTERFACE
   procedure BuildMasterHandlingTests;
 
 implementation
 
-procedure BuildMasterHandlingTests;
-begin
+uses
+  Mahogany,
+  txMeta,
+{$IFDEF USE_DLL}
+  txImports;
+{$ENDIF}
+{$IFNDEF USE_DLL}
+  xeMasters;
+{$ENDIF}
 
+procedure BuildMasterHandlingTests;
+var
+  h: Cardinal;
+begin
+  {$IF false}
+  Describe('Master Handling Functions', procedure
+    begin
+      Describe('AddMaster', procedure
+        begin
+          It('Should return true if it succeeds', procedure
+            begin
+              ExpectSuccess(AddMaster(3, 'xtest-1.esp');
+            end);
+        end);
+    end);
+  {$IFEND}
 end;
 
 end.
