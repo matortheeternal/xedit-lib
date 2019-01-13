106b2a215baed0d893c5c0af7e890b3cd0aa0902
Colin <why101@msn.com>
2019-01-13 14:39:33 -0800

    fixed StoreImageBytes/GetBitmapResource

--- src/xeTextures.pas
+++ src/xeTextures.pas
@@ -17,17 +17,23 @@ uses
 
 procedure StoreImageBytes(img: TImageData);
 var
-  ms: TMemoryStream;
+  x, y, offset: Integer;
+  rect: TColor32Rec;
+  bitmap: TImageData;
 begin
-  ms := TMemoryStream.Create;
-  try
-    if SaveImageToStream('BMP', ms, img) then begin
-      ms.Position := 0;
-      ms.Write(resultBytes, Length(resultBytes));
+  SetLength(resultBytes, img.Width * img.Height * 4);
+  InitImage(bitmap);
+  CloneImage(img, bitmap);
+  ConvertImage(bitmap, ifA8R8G8B8);
+  for y := 0 to Pred(bitmap.Height) do
+    for x := 0 to Pred(bitmap.Width) do begin
+      rect := GetPixel32(bitmap, x, y);
+      offset := (y * bitmap.Width + x) * 4;
+      resultBytes[offset]     := rect.R;
+      resultBytes[offset + 1] := rect.G;
+      resultBytes[offset + 2] := rect.B;
+      resultBytes[offset + 3] := rect.A;
     end;
-  finally
-    ms.Free;
-  end;
 end;
 
 function GetBitmapResource(resourceName: PWideChar; width, height: PInteger): WordBool; cdecl;
